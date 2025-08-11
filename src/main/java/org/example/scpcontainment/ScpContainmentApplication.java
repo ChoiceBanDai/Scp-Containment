494218e157d02ca48bde9998beb06c6d86e21f8b
antonie <3329517659@qq.com>
2025-08-11 18:09:38 +0800

    创建项目

---
+++ src/main/java/org/example/scpcontainment/ScpContainmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.scpcontainment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScpContainmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScpContainmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/scpcontainment/ScpContainmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.scpcontainment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScpContainmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
